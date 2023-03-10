20b71be0b75ae925a2d3598a6409004f04581c49
Sebastien Garcia <SebGarcia@MSI>
2023-03-10 16:23:49 +0100

    seb desespere

--- blog/resources/views/registration.blade.php
+++ blog/resources/views/registration.blade.php
@@ -4,8 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   git 
     <title>User Registration</title>
 </head>
 
